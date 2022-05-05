35469a1ef9601253a0cb1bb1062bd1fa29fc090d
Juho Inkinen <juho.inkinen@helsinki.fi>
2022-05-05 12:51:27 +0300

    Support also odt, rtf, and pptx files

--- ai.finto.fi/static/js/annif.js
+++ ai.finto.fi/static/js/annif.js
@@ -89,7 +89,7 @@ function readInput(input) {
     }
 }
 
-const supportedFormats = ['txt', 'pdf', 'doc', 'docx', 'epub', 'pptx'];
+const supportedFormats = ['txt', 'pdf', 'doc', 'docx', 'odt', 'rtf', 'pptx', 'epub'];
 
 function checkFormatSupport(extension) {
     if (!supportedFormats.includes(extension)) {
