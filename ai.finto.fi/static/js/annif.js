97694a90aa07f63bacbdb4572e33d6c6fad4cc84
Juho Inkinen <juho.inkinen@helsinki.fi>
2022-07-07 16:44:34 +0300

    Cleanup some unnecessary comment tags

--- ai.finto.fi/static/js/annif.js
+++ ai.finto.fi/static/js/annif.js
@@ -56,10 +56,10 @@ function showResults(data) {
                 $('<a id="uri-link" class="my-2" target="_blank">').attr('href',value.uri).append(value.label),
                 $('<div class="btn-group" role="group" id="copy-buttons"> \
                     <button type="button" class="btn btn-secondary copy-button" id="copy-button-label" data-i18n="[title]copy-button-label" \
-                        onclick="copyLabelToClipboard(this);"></button><!-- \
-                    --><button type="button" class="btn btn-secondary copy-button" id="copy-button-uri" data-i18n="[title]copy-button-uri" \
-                        onclick="copyUriToClipboard(this);"></button><!-- \
-                    --><button type="button" class="btn btn-secondary copy-button" id="copy-button-label-and-uri" data-i18n="[title]copy-button-label-and-uri" \
+                        onclick="copyLabelToClipboard(this);"></button> \
+                    <button type="button" class="btn btn-secondary copy-button" id="copy-button-uri" data-i18n="[title]copy-button-uri" \
+                        onclick="copyUriToClipboard(this);"></button> \
+                    <button type="button" class="btn btn-secondary copy-button" id="copy-button-label-and-uri" data-i18n="[title]copy-button-label-and-uri" \
                         onclick="copyUriAndLabelToClipboard(this);"></button> \
                 </div></li>')
             )
